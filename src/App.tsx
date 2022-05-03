8d0131077fac2d374ffa4712847c70eb9cf35ea6
MrMahanK <104770640+MrMahanK@users.noreply.github.com>
2022-05-03 17:44:41 +0430

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Header from './Header'
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
